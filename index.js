89e116c7929c5067f2182f8a70887cbd611d5415
dtica37 <diegotica@yahoo.com>
2023-11-15 16:03:29 +0200

    Uploading the APIKey

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-console.log('hello world');
+let movieNameRef = document.getElementById("movie-name");
+let searchButton = document.getElementById("search-button");
+let result = document.getElementById("result");
+
+let getMovie = () => {
+    let movieName = movieNameRef.value;
+    //let key = '';
+    let url = `https://www.omdbapi.com/?t=${movieName}&apikey=${apikey}`;
+
+    if (movieName.length <= 0) {
+        result.innerHTML = '<h3 class="msg">Please enter a movie or series name </h3>';
+    } else {
+        fetch(url)
+            .then((resp) => resp.json())
+            .then((data) => {
+                if (data.Response == "True") {
+                    result.innerHTML = `
+                        <div class="info">
+                            <img src=${data.Poster} class="poster">
+                            <div>
+                                <h2>${data.Title}</h2>
+                                <div class="rating">
+                                    <img src="star.png">
+                                    <h4>${data.imdbRating}</h4>
+                                </div>
+                                <div class="details">
+                                    <span>${data.Rated}</span>
+                                    <span>${data.Year}</span>
+                                    <span>${data.Runtime}</span>
+                                </div>
+                                <div class="genre">
+                                    <div>#${data.Genre.split(",").join("</div><div>#")}</div>
+                                </div>
+                            </div>
+                        </div>
+                        <h3>Plot:</h3>
+                        <p>${data.Plot}</p>
+                        <h3>Cast:</h3>
+                        <p>${data.Actors}</p>
+                    `;
+                } else {
+                    result.innerHTML = `<h3 class="msg">${data.Error}</h3>`;
+                }
+            })
+            .catch((error) => {
+                result.innerHTML = '<h3 class="msg">Error occurred</h3>';
+                console.error('Fetch error:', error);
+            });
+    }
+}
+
+
+searchButton.addEventListener("click", getMovie);
+window.addEventListener("load", getMovie)
+
